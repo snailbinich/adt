a40ef431c244d9f919bbdade87c8654829f61b73
snail <joeybinich@gmail.com>
2013-02-22 17:21:51 +0800

    add files

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,25 @@
-function DefaultCtrl($scope){
+function DefaultCtrl($scope, $http){
+    $scope.protocol = "http://",
+    $scope.requestUrl = "noah.baidu.com/service-tree/v1/node/1",
+    $scope.responseSource = "",
 
     $scope.sendRequest = function(){
-       console.log(requestUrl);
+       $http({method: 'GET', url: $scope.protocol+$scope.requestUrl}).
+       success(function(data, status, headers, config) {
+         console.log(data);
+         console.log(status);
+         console.log(headers);
+         console.log(config);
+         $scope.responseSource = data;
+         }).
+       error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+         console.log("errors");
+         console.log(data);
+         console.log(status);
+         console.log(headers);
+         console.log(config);
+       });
     }
 }
